9f802c91c90ecd09ed37fb4890ee07984408dd97
santiago cano duque <cano2030@gmail.com>
2022-10-12 17:40:57 -0500

    Modificacion en parametro del modelo
    - Se altera el atributo de "Hobby" para libertad del usuario. Se elimnan las choices de dos parametros para que el usuario pueda escribir lo que desee.

--- crud/models.py
+++ crud/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     doc = models.CharField(max_length=50)
     name = models.CharField(max_length=100)
     surnames = models.CharField(max_length=100)
-    hobbie = models.CharField(max_length=10)
+    hobbie = models.CharField(max_length=50)
     objects = models.Manager()
     
     class Meta:
