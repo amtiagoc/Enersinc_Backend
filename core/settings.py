49279d0d419696b7d14c49342afd6cad485c7313
santiago cano duque <cano2030@gmail.com>
2022-10-12 18:04:50 -0500

    Libreria adicionada en requierements y correcion
    - Se adiciona libreria a requierements.txt
    - Se eliminar Middleware repetidos y se instala django-cors-headers

--- core/settings.py
+++ core/settings.py
@@ -45,16 +45,6 @@ INSTALLED_APPS = [
     "corsheaders",
 ]
 
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
-
 CORS_ORIGIN_ALLOW_ALL = True
 
 
@@ -71,17 +61,6 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
-
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-]
 
 ROOT_URLCONF = "core.urls"
 
